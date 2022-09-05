e11758bde6d5b6fb4d41dc7ed17feb0fe624e1b8
arafat356 <ahmedarafatft@gmail.com>
2022-09-06 00:00:03 +0600

    nodeExpress

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 30000;
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("we're using nodemon for better experience and auto restart");
 });
 
 const users = [
-    { id: 1, name: 'future', email: 'future@gmail.com', phone: '85430985' },
+    { id: 0, name: 'future', email: 'future@gmail.com', phone: '85430985' },
     {
         id: 1,
         name: 'future loading',
         email: 'future@gmail.com',
         phone: '85430985',
     },
-    { id: 1, name: 'nothing', email: 'future@gmail.com', phone: '85430985' },
+    { id: 2, name: 'nothing', email: 'future@gmail.com', phone: '85430985' },
     {
-        id: 1,
+        id: 3,
         name: 'i heat my life',
         email: 'future@gmail.com',
         phone: '85430985',
     },
-    { id: 2, name: 'cool', email: 'future@gmail.com', phone: '85430985' },
-    { id: 3, name: 'stay alone', email: 'future@gmail.com', phone: '85430985' },
-    { id: 4, name: 'no past tan', email: 'future@gmail.com', phone: '85430985' },
-    { id: 5, name: 'no love', email: 'future@gmail.com', phone: '85430985' },
-    { id: 6, name: 'no emotion', email: 'future@gmail.com', phone: '85430985' },
-    { id: 7, name: 'dont friend', email: 'future@gmail.com', phone: '85430985' },
+    { id: 4, name: 'cool', email: 'future@gmail.com', phone: '85430985' },
+    { id: 5, name: 'stay alone', email: 'future@gmail.com', phone: '85430985' },
+    { id: 6, name: 'no past tan', email: 'future@gmail.com', phone: '85430985' },
+    { id: 7, name: 'no love', email: 'future@gmail.com', phone: '85430985' },
+    { id: 8, name: 'no emotion', email: 'future@gmail.com', phone: '85430985' },
+    { id: 9, name: 'dont friend', email: 'future@gmail.com', phone: '85430985' },
 ];
 
 app.get('/users', (req, res) => {
@@ -42,6 +46,18 @@ app.get('/users', (req, res) => {
     res.send(users);
 });
 
+/* app.method */
+
+app.post('/users', (req, res) => {
+    const NewUser = req.body;
+    NewUser.id = users.length;
+    users.push(NewUser);
+    console.log('user loading', req.body);
+    // res.send(JSON.stringify(NewUser));
+    res.json(NewUser);
+});
+
+/* dynamic api */
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
     const user = users[id];
